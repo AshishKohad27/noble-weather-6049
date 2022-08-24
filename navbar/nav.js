e4b85dee86779d26e5666f39ac19edeeb2cc9230
santosh yadav <styadav412@gamil.com>
2022-08-24 19:42:20 +0530

    added navbar

---
+++ navbar/nav.js
@@ -0,0 +1,53 @@
+count=1
+let show=()=>{
+    let close=document.querySelector(".close")
+    count++;
+    if(count%2==0){
+    close.style.display="block"
+}
+else{
+close.style.display="none"
+}
+    
+}
+count2=1
+let down=()=>{
+let close=document.querySelector(".dow")
+    count2++;
+    if(count2%2==0){
+        close.style.display="block"
+
+}
+else{
+    close.style.display="none"
+}
+
+}
+
+count3=1
+let down1=()=>{
+let close=document.querySelector(".down1")
+    count2++;
+    if(count2%2==0){
+        close.style.display="block"
+
+}
+else{
+    close.style.display="none"
+}
+
+}
+
+count4=1
+let down2=()=>{
+let close=document.querySelector(".down2")
+    count2++;
+    if(count2%2==0){
+        close.style.display="block"
+
+}
+else{
+    close.style.display="none"
+}
+
+}
