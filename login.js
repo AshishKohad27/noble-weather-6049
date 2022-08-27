b75d605aa02e64c53b3eb08d6eb46e4f67e98a6d
shubhambhoskar <shubhambhoskar@gmail.com>
2022-08-27 12:17:05 +0530

    changes

--- login.js
+++ login.js
@@ -7,10 +7,13 @@ function log(){
     // console.log(dataarr)
     let loginemail=document.getElementById("checkemail").value;
     let loginpass=document.getElementById("checkpass").value;
+
+    
     let flag=0;
     for(let i=0;i<dataarr.length;i++){
         if(dataarr[i].email=loginemail && dataarr[i].pass==loginpass){
              flag=1;
+            //  localStorage.setItem("logindata",JSON.stringify(loginemail));
             break;
         }
     
